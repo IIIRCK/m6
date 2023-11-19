58e0d6e230f74fefd01fa92dfae6c5a47140a84b
Rackham <juanmanuelmg003@gmail.com>
2023-11-19 19:05:30 +0100

    novo

--- uf2/act01/act01.js
+++ uf2/act01/act01.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     localStorage.setItem('usr', '1234');
     localStorage.setItem('usr1', '1234');
-    //loggin()
-    plane();
+    loggin()
+    //plane();
     //localStorage.clear()
 });
 
 let cnt = document.querySelector('.container');
 let user
 
-let s_id  = [];
-let s_st  =[]
-let us_id = [];
-let us_st = []
+let plane_seats  = [];
+let user_seats = [];
 function loggin(){
     let loggin = document.createElement('div');
     loggin.setAttribute('class', 'loggin');
@@ -41,8 +39,11 @@ function check_loggin(a,b){
         cnt.innerHTML=''
         user = a;
         plane();
+        plane_seats =[];
+        user_seats =[];
         load_user_seats(user)
-        load_seats();
+        load_plane_seats();
+
     }
     else {
         alert('usr o psswd no coincide')
@@ -83,10 +84,8 @@ function plane(){
                     s_ac.appendChild(btn)
                     if (j===1){
                        btn_set(btn,'a',i)
-
                     }else if (j===2){
                         btn_set(btn,'b',i)
-
 
                     }else if (j===3){
                         btn_set(btn,'c',i)
@@ -135,13 +134,12 @@ function btn_set(btn,lt,i){
 }
 
 function add_seat(button){
-    let id = button.id
-    btn_seat_color(button)
-    add_seat_array(button)
+    btn_set_color(button)
+    add_set_array(button)
 
 }
 
-function btn_seat_color(btn){
+function btn_set_color(btn){
     let st= btn.getAttribute('st')
     if (st ==='0'){
         btn.style.backgroundColor = 'yellow';
@@ -154,86 +152,69 @@ function btn_seat_color(btn){
         btn.style.color= 'white'
     }
     else if (st === '3'){
-        btn.style.backgroundColor = 'green';
+        btn.style.backgroundColor = 'red';
+        btn.disabled = 'true'
     }
 
 }
 
-function btn_save(){
-    let s_u='';
-    let s_a = ''
-    us_id.forEach(i => {
-        s_u+= i+';'
-        let c = s_id.find((e) => e===i)
-        if (c){
-            s_id.push(i)
-        }
 
-    })
-    s_id.forEach(i =>{
-        s_a += i+';'
-    })
-    localStorage.setItem(user+'_s_id',s_u)
-    localStorage.setItem('s_id',s_u)
-    let array = s_u.split(';')
-    array.pop()
-
-
-}
-
-function btn_exit(){
-cnt.innerHTML = ''
-    loggin()
-    us_id =[];
-    s_id=[];
-}
-
-function add_seat_array(btn){
-    let st = btn.getAttribute('st')
+function add_set_array(btn){
     let id = btn.id;
-    let c = us_id.find((e) =>e===id)
-    let p = us_id.indexOf(c)
-    if (c){
-        us_id.splice(p,1)
-        us_st.splice(p,1)
-    }else{
-        us_id.push(id)
-        us_st.push(st)
+    let i = user_seats.indexOf(id);
+    let ii = plane_seats.indexOf(id)
+    if (i !== -1 && ii !== -1) {
+        user_seats.splice(i, 1);
+        plane_seats.splice(ii,1)
+    } else {
+        user_seats.push(id);
+        plane_seats.push(id)
     }
 
 }
 
 function load_user_seats(u){
-    let s = localStorage.getItem(u+'_s_id')
-    let s_u = s.split(';')
-    s_u.pop();
-    s_u.forEach(i =>{
-        let k = '#'+i;
-        let btn = document.getElementById(i)
-        btn.style.backgroundColor = 'yellow'
-        btn.style.color = 'black'
-        btn.setAttribute('st','1')
-    })
-
-}
-
-function load_seats(){
-    let s = localStorage.getItem('s_id')
-    let s_p = s.split(';')
-    s_p.pop();
-    s_p.forEach(i =>{
-        if (us_id.find((e) => e===i)){
-            let k = '#'+i;
+    let s = localStorage.getItem(u + '_seats')
+    if ( s!== null) {
+        let s_u = s.split(',')
+        s_u.pop();
+        s_u.forEach(i => {
+            user_seats.push(i);
             let btn = document.getElementById(i)
-            btn.style.backgroundColor = 'red'
-            btn.style.color = 'white'
-            btn.setAttribute('st','1')
-            btn.disabled = true;
-        }
-
-
-    })
-
+            btn_set_color(btn)
+        })
+    }
 }
 
-
+function load_plane_seats(){
+    let s = localStorage.getItem('plane_seats')
+    if (s !== null) {
+        let s_p = s.split(',')
+        s_p.pop();
+        s_p.forEach(i => {
+            let ii = user_seats.indexOf(i)
+            if (ii === -1) {
+                let btn = document.getElementById(i)
+                btn.setAttribute('st', '3')
+                btn_set_color(btn)
+            }
+            plane_seats.push(i)
+        })
+    }
+}
+function btn_save(){
+    let s_u=''
+    let s_p = ''
+    user_seats.forEach(i => {
+        s_u+= i+','
+    })
+    plane_seats.forEach(i =>{
+        s_p += i+','
+    })
+    localStorage.setItem(user+'_seats',s_u)
+    localStorage.setItem('plane_seats',s_p)
+}
+function btn_exit(){
+    cnt.innerHTML = ''
+    loggin()
+}
